c80db376132ce8e3ed81bf837c5899560fee8872
Kagheni <Kaghenimbale@gmail.com>
2023-07-27 11:15:21 +0200

    Add social media icons

--- app/javascript/components/Navbar/Navbar.js
+++ app/javascript/components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { ImFacebook } from 'react-icons/im';
+import { AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { TfiPinterest } from 'react-icons/tfi';
 
 const Navbar = () => {
   const navlist = [
-    'Items',
-    'Reserve Form',
-    'My Reservation',
-    'Add Item',
-    'Delete Item',
+    'ITEMS',
+    'RESERVE FORM',
+    'MY RESERVATION',
+    'ADD ITEM',
+    'DELETE ITEM',
   ];
   return (
     <div className="navbar">
@@ -20,7 +24,13 @@ const Navbar = () => {
           ))}
         </ul>
       </div>
-      <div className="social">Social Media Links</div>
+      <div className="social">
+        <AiOutlineTwitter className="icon" />
+        <ImFacebook className="icon" />
+        <FaLinkedinIn className="icon" />
+        <AiFillInstagram className="icon" />
+        <TfiPinterest className="icon" />
+      </div>
     </div>
   );
 };
