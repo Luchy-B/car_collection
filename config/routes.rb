a9ba686d6cda516438be2ad1b01067d566ba99b5
evillaz <evillaz123@gmail.com>
2023-07-27 08:01:33 -0500

    Add api endpoints for cars reservations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'root#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :cars
+      resources :reservations
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
