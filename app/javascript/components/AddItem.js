0c2b8c16e50fb75f29de1bf09d054e632facac91
david30-maker <davidokpah396@gmail.com>
2023-08-01 10:33:08 +0100

    fix linter error

--- app/javascript/components/AddItem.js
+++ app/javascript/components/AddItem.js
@@ -31,39 +31,39 @@ const AddItem = () => {
       [name]: value,
     }));
   };
-  
+
   return (
     <div className="addItem_container">
       <form onSubmit={handleSubmit}>
         <h3 className="add-form-title">Add an Item</h3>
         <div className="form-group">
           <label htmlFor="name">
-           
-            <input type="text" name="name" value={carData.name} onChange={handleChange} required placeholder="Name"/>
+
+            <input type="text" name="name" value={carData.name} onChange={handleChange} required placeholder="Name" />
           </label>
           <label htmlFor="description">
-            
-            <input type="text" name="description" value={carData.description} onChange={handleChange} required placeholder="Description"/>
+
+            <input type="text" name="description" value={carData.description} onChange={handleChange} required placeholder="Description" />
           </label>
           <label htmlFor="finance_fee">
-          
-            <input type="number" name="finance_fee" value={carData.finance_fee} onChange={handleChange} required placeholder="Finance Fee"/>
+
+            <input type="number" name="finance_fee" value={carData.finance_fee} onChange={handleChange} required placeholder="Finance Fee" />
           </label>
           <label htmlFor="purchase_fee">
-           
-            <input type="number" name="purchase_fee" value={carData.purchase_fee} onChange={handleChange} required placeholder="Purchase Fee"/>
+
+            <input type="number" name="purchase_fee" value={carData.purchase_fee} onChange={handleChange} required placeholder="Purchase Fee" />
           </label>
           <label htmlFor="total_amount">
-           
-            <input type="number" name="total_amount" value={carData.total_amount} onChange={handleChange} required placeholder="Total Amount"/>
+
+            <input type="number" name="total_amount" value={carData.total_amount} onChange={handleChange} required placeholder="Total Amount" />
           </label>
           <label htmlFor="apr">
-          
-            <input type="number" name="apr" value={carData.apr} onChange={handleChange} placeholder="APR"/>
+
+            <input type="number" name="apr" value={carData.apr} onChange={handleChange} placeholder="APR" />
           </label>
           <label htmlFor="snapshot">
-           
-            <input type="file" name="snapshot" onChange={handleChange} placeholder="Snapshot"/>
+
+            <input type="file" name="snapshot" onChange={handleChange} placeholder="Snapshot" />
           </label>
         </div>
         <button type="submit" className="btn_add_item">Add New Car</button>
