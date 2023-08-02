b1b0fbd986450672ddb5633b2efd871913665a28
david30-maker <davidokpah396@gmail.com>
2023-08-03 00:43:40 +0100

    ADD:styling reservation link component

--- app/javascript/components/Reservation.js
+++ app/javascript/components/Reservation.js
@@ -3,41 +3,41 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservations } from '../redux/reservationsSlice';
 
 const Reservation = () => {
-    const dispatch = useDispatch();
-    const { data: reservations, loading, error } = useSelector(
-        (state) => state.reservation
-    );
-    
-    useEffect(() => {
-        dispatch(fetchReservations());
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  const { data: reservations, loading, error } = useSelector(
+    (state) => state.reservations
+  );
 
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+  useEffect(() => {
+    dispatch(fetchReservations());
+  }, [dispatch]);
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-    return (
-        <div className="reserve_link_container">
-            <h2>My Reservations</h2>
-            {reservations && reservations.length > 0 ? (
-                <ul>
-                    {reservations.map((reservation) => (
-                        <li key={reservation.id}>
-                            <p>Name: {reservation.car_name}</p>
-                            <p>Date: {reservation.date}</p>
-                            <p>City: {reservation.city}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No reservations found.</p>
-            )}
-        </div>
-    );
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className="reserve_link_container">
+      <h2>My Reservations</h2>
+      {reservations && reservations.length > 0 ? (
+        <ul>
+          {reservations.map((reservation) => (
+            <li key={reservation.id}>
+              <p>Name: {reservation.car_name}</p>
+              <p>Date: {reservation.date}</p>
+              <p>City: {reservation.city}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No reservations found.</p>
+      )}
+    </div>
+  );
 };
 
 export default Reservation;
--- app/javascript/redux/reservationsSlice.js
+++ app/javascript/redux/reservationsSlice.js
@@ -3,32 +3,35 @@ import axios from "axios";
 
 const API_URL = 'http://localhost:3000/api/v1/reservations';
 
-export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async (_, thunkAPI) => {
-    try {
+export const fetchReservations = createAsyncThunk(
+    'reservations/fetchReservations',
+    async (_, thunkAPI) => {
+      try {
         const response = await axios.get(API_URL);
         console.log('API Response:', response.data);
-        return response.data;
-    } catch (error) {
-       return thunkAPI.rejectWithValue(error.response);
+        return response.data; 
+      } catch (error) {
+        console.error('API Error:', error);
+        return thunkAPI.rejectWithValue('Failed to fetch reservations');
+      }
     }
-    }
-);
+  );  
 
 const initialReservations = [
     {
-      id: 1,
-      car_name: "Item 1",
-      date: "2023-08-02",
-      city: "City 1",
+      "id": 1,
+      "car_name": "Item 1",
+      "date": "2023-08-02",
+      "city": "City 1"
     },
     {
-      id: 2,
-      car_name: "Item 2",
-      date: "2023-08-03",
-      city: "City 2",
-    },  
+      "id": 2,
+      "car_name": "Item 2",
+      "date": "2023-08-03",
+      "city": "City 2"
+    },
   ];
-
+  
 const reservationsSlice = createSlice({
     name: 'reservations',
     initialState: {
