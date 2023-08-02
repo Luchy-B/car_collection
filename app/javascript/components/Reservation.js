03e8cc58fef2da5944e90461a0e3a5df4851c008
david30-maker <davidokpah396@gmail.com>
2023-08-02 13:44:24 +0100

    ADD:implement reservation link component

--- app/javascript/components/Reservation.js
+++ app/javascript/components/Reservation.js
@@ -1,5 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../redux/reservationsSlice';
 
-const Reservation = () => <div>Reservation</div>;
+
+const Reservation = () => {
+    const dispatch = useDispatch();
+    const { data: reservations, loading, error } = useSelector(
+        (state) => state.reservations
+    );
+
+    useEffect(() => {
+        dispatch(fetchReservations());
+    }, [dispatch]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+
+
+    return (
+        <div>
+            <h1>Reservations</h1>
+            <ul>
+                {reservations.map((reservation) => (
+                    <li key={reservation.id}>
+                        <p>Name: {reservation.name}</p>
+                        <p>City: {reservation.city}</p>
+                        <p>Date: {reservation.date}</p>
+                        </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
 
 export default Reservation;
