68aac3ee35ece189627734f7c6aabaca28618645
david30-maker <davidokpah396@gmail.com>
2023-08-02 13:31:44 +0100

    ADD:implement store for reservation link component

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import carsReducer from './redux/carsSlice';
+import reservationsReducer from './redux/reservationsSlice';
 
 const store = configureStore({
   reducer: {
     car: carsReducer,
+    reservation: reservationsReducer,
   },
 });
 
