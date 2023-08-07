94d7148ec8b20b02d2281cdad71929b4617011d7
evillaz <evillaz123@gmail.com>
2023-08-06 22:49:21 -0500

    Create main page ui

--- app/javascript/components/Mainpage.js
+++ app/javascript/components/Mainpage.js
@@ -1,5 +1,137 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-const Mainpage = () => <div>Mainpage</div>;
+import { ImFacebook } from 'react-icons/im';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { CiInstagram } from 'react-icons/ci';
+import { RxTrackNext, RxTrackPrevious } from 'react-icons/rx';
 
-export default Mainpage;
+import { getCars } from '../redux/Cars/carsSlice';
+
+export const Main = () => {
+  const { cars, isLoading } = useSelector((store) => store.cars);
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1200) {
+        setItemsPerPage(3);
+      } else if (window.innerWidth > 480) {
+        setItemsPerPage(2);
+      } else if (window.innerWidth <= 480) {
+        setItemsPerPage(1);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const totalItems = cars.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
+  const indexOfLastItem = (currentPage * itemsPerPage);
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentPageCars = cars.slice(indexOfFirstItem, indexOfLastItem);
+
+  useEffect(() => {
+    if (cars.length === 0) {
+      dispatch(getCars());
+    }
+  }, [cars.length, dispatch]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="main-page f r">
+      <button
+        type="button"
+        disabled={currentPage === 1}
+        onClick={() => handlePageChange(currentPage - 1)}
+        className={`pagination prev f c ${currentPage === 1 ? 'disabled' : 'active'}`}
+      >
+        <RxTrackPrevious className="pagination-previous" />
+      </button>
+      <div className="main-container f c">
+        <div className="main-header f c">
+          <h2>
+            LATEST MODELS
+          </h2>
+          <span>
+            Please select a Car Model
+          </span>
+          <span className="main-header-underline" />
+        </div>
+        <div className="vehicles-container f r">
+          {currentPageCars.map((car) => (
+            <div key={car.id} className="vehicle-holder main-content f c">
+              <div className="vehicle-circle mainImg f">
+                <NavLink to={`/DETAILS/${car.id}`}>
+                  <img className="vehicle" src={car.snapshot_url} alt="categoryIcon" />
+                </NavLink>
+                <div className="background-holder" />
+              </div>
+
+              <div className="main-description f c">
+                <h3 className="vehicle-model">
+                  { car.name}
+                </h3>
+                <p className="vehicle-description">
+                  { car.description }
+                </p>
+                <div className="vehicle-icons f r">
+                  <a
+                    className="facebook-icon i f"
+                    href="*"
+                  >
+                    <ImFacebook className="vehicle-icon" />
+                  </a>
+                  <a
+                    className="twitter-icon i f"
+                    href="*"
+                  >
+                    <AiOutlineTwitter className="vehicle-icon" />
+                  </a>
+                  <a
+                    className="instagram-icon i f"
+                    href="*"
+                  >
+                    <CiInstagram className="vehicle-icon" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <button
+        type="button"
+        disabled={currentPage === totalPages}
+        onClick={() => handlePageChange(currentPage + 1)}
+        className={`pagination next f c ${currentPage === totalPages ? 'disabled' : 'active'}`}
+      >
+        <RxTrackNext className="pagination-next" />
+      </button>
+    </div>
+  );
+};
+
+export default Main;
