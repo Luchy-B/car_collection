1108d461b38c843a6f9f280d0c58b3022afe69a6
evillaz <evillaz123@gmail.com>
2023-08-08 02:58:12 -0500

    Correct linters

--- app/javascript/components/Details.js
+++ app/javascript/components/Details.js
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { TfiArrowCircleRight } from 'react-icons/tfi';
 import { RxTrackPrevious } from 'react-icons/rx';
 import { getCar } from '../redux/Cars/carsSlice';
-import { NavLink } from 'react-router-dom';
 
 const Details = () => {
   const dispatch = useDispatch();
@@ -22,7 +21,7 @@ const Details = () => {
       && (
       <>
         <div className="detail-container f c">
-          <div className='details-holder f'>  
+          <div className="details-holder f">
             <div className="detailsPage1">
               <div className="detailImg">
                 <img className="detailVehicle" src={car.snapshot_url} alt="categoryIcon" />
@@ -41,23 +40,43 @@ const Details = () => {
               <ul className="car-details">
                 <li>
                   <p>Finance Fee:</p>
-                  <p className="car-detail">${car.finance_fee}</p>
+                  <p className="car-detail">
+                    $
+                    {car.finance_fee}
+                  </p>
                 </li>
                 <li>
                   <p>Purchase Fee:</p>
-                  <p className="car-detail">${ car.purchase_fee }</p>
+                  <p className="car-detail">
+                    $
+                    { car.purchase_fee }
+                  </p>
                 </li>
                 <li>
                   <p>total_amount:</p>
-                  <p className="car-detail">${ car.total_amount }</p>
+                  <p className="car-detail">
+                    $
+                    { car.total_amount }
+                  </p>
                 </li>
                 <li>
                   <p>Duration:</p>
-                  <p className="car-detail">{ car.duration } Months</p>
+                  <p className="car-detail">
+                    { car.duration }
+                    {' '}
+                    Months
+                  </p>
                 </li>
               </ul>
-              <div className='apr-detail'>
-                <p className="car-detail"><span>{ car.apr }% APR </span>Representative</p>
+              <div className="apr-detail">
+                <p className="car-detail">
+                  <span>
+                    { car.apr }
+                    % APR
+                    {' '}
+                  </span>
+                  Representative
+                </p>
               </div>
               <NavLink to={`/RESERVE_FORM/${car.id}`}>
                 <div className="car-reserve">
@@ -68,10 +87,10 @@ const Details = () => {
             </div>
           </div>
           <div className="detailsBack-container">
-            <NavLink to={`/`}>
+            <NavLink to="/">
               <RxTrackPrevious className="detailsBack" />
             </NavLink>
-            </div>
+          </div>
         </div>
       </>
       )}
