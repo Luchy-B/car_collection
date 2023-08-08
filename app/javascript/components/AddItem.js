41a25a005b923bdd6ca70c3a762762b9ac0e47f4
david30-maker <davidokpah396@gmail.com>
2023-08-08 13:37:25 +0100

    ADD:implement requested changes

--- app/javascript/components/AddItem.js
+++ app/javascript/components/AddItem.js
@@ -1,32 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { addCar } from '../redux/Cars/carsSlice';
 
 const AddItem = () => {
   const dispatch = useDispatch();
 
-  const [name, setName] = useState(null);
-  const [description, setDescription] = useState(null);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
   const [financeFee, setFinanceFee] = useState(0);
   const [purchaseFee, setPurchaseFee] = useState(0);
-  const [totalAmount, setTotalAmount] = useState(0);
+  const [totalAmount, setTotalAmount] = useState(purchaseFee);
   const [duration, setDuration] = useState(1);
   const [apr, setApr] = useState(0);
   const [snapshot, setSnapshot] = useState(null);
 
+  useEffect(() => {
+    setTotalAmount(parseFloat(purchaseFee) + parseFloat(financeFee));
+  }, [purchaseFee, financeFee]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const carData = new FormData();
-    carData.append('car[name]', name);
-    carData.append('car[description]', description);
-    carData.append('car[finance_fee]', financeFee);
-    carData.append('car[purchase_fee]', purchaseFee);
-    carData.append('car[total_amount]', totalAmount);
-    carData.append('car[duration]', duration);
-    carData.append('car[apr]', apr);
-    carData.append('car[snapshot]', snapshot);
-    await dispatch(addCar(carData));
-    window.location.reload();
+    if (totalAmount >= purchaseFee) {
+      const carData = new FormData();
+      carData.append('car[name]', name);
+      carData.append('car[description]', description);
+      carData.append('car[finance_fee]', financeFee);
+      carData.append('car[purchase_fee]', purchaseFee);
+      carData.append('car[total_amount]', totalAmount);
+      carData.append('car[duration]', duration);
+      carData.append('car[apr]', apr);
+      carData.append('car[snapshot]', snapshot);
+      await dispatch(addCar(carData));
+      window.location.reload();
+    }
   };
 
   return (
@@ -35,40 +41,110 @@ const AddItem = () => {
         <h3 className="add-form-title">Add an Item</h3>
         <div className="form-group">
           <label htmlFor="name">
-
-            <input type="text" name="name" onChange={(e) => setName(e.target.value)} required placeholder="Name" className="add_item_input" />
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              placeholder="Name"
+              className="add_item_input"
+            />
           </label>
           <label htmlFor="description">
-
-            <input type="text" name="description" onChange={(e) => setDescription(e.target.value)} required placeholder="Description" className="add_item_input" />
+            Description:
+            <input
+              type="text"
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+              placeholder="Description"
+              className="add_item_input"
+            />
           </label>
           <label htmlFor="finance_fee">
-
-            <input type="number" name="finance_fee" onChange={(e) => setFinanceFee(e.target.value)} required placeholder="Finance Fee" className="add_item_input" />
+            Finance Fee:
+            <input
+              type="number"
+              name="finance_fee"
+              value={financeFee}
+              onChange={(e) => setFinanceFee(e.target.value)}
+              required
+              placeholder="Finance Fee"
+              className="add_item_input"
+            />
           </label>
           <label htmlFor="purchase_fee">
-
-            <input type="number" name="purchase_fee" onChange={(e) => setPurchaseFee(e.target.value)} required placeholder="Purchase Fee" className="add_item_input" />
+            Purchase Fee:
+            <input
+              type="number"
+              name="purchase_fee"
+              value={purchaseFee}
+              onChange={(e) => setPurchaseFee(e.target.value)}
+              required
+              placeholder="Purchase Fee"
+              className="add_item_input"
+            />
           </label>
           <label htmlFor="total_amount">
-
-            <input type="number" name="total_amount" onChange={(e) => setTotalAmount(e.target.value)} required placeholder="Total Amount" className="add_item_input" />
+            Total Amount:
+            <input
+              type="number"
+              name="total_amount"
+              value={totalAmount}
+              onChange={(e) => setTotalAmount(e.target.value)}
+              required
+              placeholder="Total Amount"
+              className="add_item_input"
+              disabled
+            />
           </label>
-
           <label htmlFor="duration">
-            <input type="number" name="duration" onChange={(e) => setDuration(e.target.value)} required placeholder="duration" className="add_item_input" />
+            Duration:
+            <input
+              type="number"
+              name="duration"
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+              required
+              placeholder="Duration"
+              className="add_item_input"
+            />
           </label>
-
           <label htmlFor="apr">
-
-            <input type="number" name="apr" onChange={(e) => setApr(e.target.value)} placeholder="APR" className="add_item_input" />
+            APR:
+            <input
+              type="number"
+              name="apr"
+              value={apr}
+              onChange={(e) => setApr(e.target.value)}
+              placeholder="APR"
+              className="add_item_input"
+            />
           </label>
           <label htmlFor="snapshot">
-
-            <input type="file" name="snapshot" onChange={(e) => setSnapshot(e.target.files[0])} placeholder="Snapshot" className="add_item_input" />
+            Snapshot:
+            <input
+              type="file"
+              name="snapshot"
+              onChange={(e) => setSnapshot(e.target.files[0])}
+              placeholder="Snapshot"
+              className="add_item_input"
+            />
           </label>
         </div>
-        <button type="submit" className="btn_add_item">Add New Car</button>
+        {totalAmount < purchaseFee && (
+          <p>Total amount must be greater than or equal to purchase fee.</p>
+        )}
+        <button
+          type="submit"
+          className="btn_add_item"
+          disabled={totalAmount < purchaseFee}
+        >
+          Add New Car
+        </button>
       </form>
     </div>
   );
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,13 @@
 class Car < ApplicationRecord
   has_many :reservations
   has_one_attached :snapshot
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :finance_fee, presence: true
+  validates :purchase_fee, presence: true
+  validates :total_amount, presence: true
+  validates :duration, presence: true
+  validates :apr, presence: true
+  validates :snapshot, presence: true
 end
